fb81c6a59f3306962fba5c0ece28986b98d69ed1
Muthu <muthu2507@gmail.com>
2022-07-16 12:37:29 +0800

    folder 4, video 38

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatefulWidget {
+  // const NewTransaction({Key key}) : super(key: key);
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+
+    Navigator.of(context).pop(); // To close modal sheet on entering submit on keyboard
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              onSubmitted: (_) => submitData(),
+              // onChanged: (value) {
+              //   titleInput = value;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+              // onChanged: (value) => amountInput = value,
+            ),
+            TextButton(
+              child: Text('Add Transaction'),
+              style: TextButton.styleFrom(primary: Colors.purple),
+              onPressed: submitData,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/user_transactions.dart
@@ -0,0 +1,29 @@
+// import 'package:flutter/src/foundation/key.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+
+// import 'package:flutter/material.dart';
+
+// import '../models/transaction.dart';
+// import 'new_transaction.dart';
+// import 'transaction_list.dart';
+
+// class UserTransactions extends StatefulWidget {
+//   // const UserTransactions({Key key}) : super(key: key);
+
+//   @override
+//   State<UserTransactions> createState() => _UserTransactionsState();
+// }
+
+// class _UserTransactionsState extends State<UserTransactions> {
+  
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         NewTransaction(_addNewTransaction),
+//         TransactionList(_userTransactions),
+//       ],
+//     );
+//   }
+// }
